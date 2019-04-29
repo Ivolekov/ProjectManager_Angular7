2e3e801e66e3ba6f5d3358a5f95b7e293b6979b2
Ivo Lekov <ivolekovbg@gmail.com>
2019-04-29 22:17:58 +0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProjectDetailsComponent } from './project-details/project-details.component';
+import{ SelectProjectComponent } from './select-project/select-project.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //{path:'', component:ProjectDetailsComponent},
+  {path:'', redirectTo:'/project', pathMatch:'full'},
+  {path:'project/:id', component:ProjectDetailsComponent},
+  {path:'**', component:SelectProjectComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [ProjectDetailsComponent, SelectProjectComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { ProjectComponent } from './project/project.component';
+import { ProjectFormComponent } from './project/project-form/project-form.component';
+import { ProjectsListComponent } from './project/projects-list/projects-list.component';
+import { TaskFormComponent } from './project-details/task-form/task-form.component';
+import { TasksListComponent } from './project-details/tasks-list/tasks-list.component';
+import { ProjectService } from './services/project/project.service';
+import { TaskService } from './services/task/task.service';
+import { SelectProjectComponent } from './select-project/select-project.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectComponent,
+    ProjectFormComponent,
+    ProjectsListComponent,
+    TaskFormComponent,
+    TasksListComponent,
+    routingComponents,
+    SelectProjectComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
   ],
-  providers: [],
+  providers: [ProjectService,TaskService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
